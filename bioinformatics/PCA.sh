602f1b5bb67d4b96315c17300165b88efade9f3e
BBriggeman <bo.briggeman@wur.nl>
2026-07-29 13:38:00 +0200

    Add explanation PCA.sh

--- bioinformatics/PCA.sh
+++ bioinformatics/PCA.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
+#First, run the initial plink on all isolates. The results will be used to make a preliminary PCA
 plink --vcf LFHF_global_final_AD_5_50000.vcf.gz \
       --make-bed \
       --out LFHF_global_filtered_plink_50000 \
@@ -12,7 +13,7 @@ plink --bfile LFHF_global_filtered_plink_50000 \
       --genome \
       --pca
 
-##The filtering will be done in this script just to keep track. 
+##The filtering will be done in this script just to keep track. The .txt files contains the file names of the outliers that will be filtered out based on PC-value (see /plotting/LFHF_global_PCA_BB.R)
 #bcftools view -S "^Remove_PCA_LFHF_global_50000_v01.txt" -Oz -o LFHF_global_final_AD_5_50000_v02.vcf.gz LFHF_global_final_AD_5_50000.vcf.gz
 #bcftools index -f LFHF_global_final_AD_5_50000_v02.vcf.gz
 
