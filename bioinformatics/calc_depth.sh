9aba5578c62e3ac86ee69fe17355717fa2c23695
BBriggeman <bo.briggeman@wur.nl>
2026-07-29 14:56:51 +0200

    Update calc_depth.sh

--- bioinformatics/calc_depth.sh
+++ bioinformatics/calc_depth.sh
@@ -1,7 +1,10 @@
+#Makes a list of all the files
 bcftools query -l LFHF_global_final_AD_5_50000.vcf.gz > all_samples.txt
 
+#Start to make the csv file, the calculations of population averages can be performed in R or Excel. 
 echo "sample,average_depth" > average_depth.csv
 
+#Calculates the average coverage based on the DP values in the combined.vcf.gz
 while read sample; do
     avg=$(bcftools query -s "$sample" -f '[%DP\n]' LFHF_global_final_AD_5_50000.vcf.gz | \
         awk '$1!="." {sum+=$1; n++} END {if(n>0) printf "%.2f", sum/n; else print "NA"}')
